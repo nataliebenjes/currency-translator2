6011a1e72c6ef172bb3a399194cd1bf01213d096
Natalie Benjes <natalie.benjes@gmail.com>
2023-07-01 15:13:50 -0700

    update form and BI to include amount

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import TranslateService from './translator.js';
 
 // Business Logic
 
-function getTranslation(phrase) {
+function getTranslation(phrase, amount) {
   console.log(phrase);
   let promise = TranslateService.getTranslation(phrase);
+  console.log()
   promise.then(function (response.conversion_rates) {
 
     printElements(response.conversion_rates.USD);
@@ -28,7 +29,8 @@ function printError(error) {
 function handleFormSubmission(event) {
   event.preventDefault();
   const phrase = document.querySelector('#entry').value;
-  getTranslation(phrase);
+  const amount = parseInt(document.querySelector("#amount").value);
+  getTranslation(phrase, amount);
 }
 
 window.addEventListener("load", function () {
